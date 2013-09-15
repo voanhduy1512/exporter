be738d60497c82da676f6b24c56d22f96c127a04
Võ Anh Duy <voanhduy1512@live.com>
2013-09-16 00:34:02 +0700

    add initialize for Document

--- lib/exporter/document.rb
+++ lib/exporter/document.rb
@@ -1,6 +1,11 @@
 module Exporter
   class Document
     attr_reader :data
+
+    def initialize(data)
+      @data = data
+    end
+    
     def to_file(path)
       raise NotImplementedError.new("You must implement this to_file method.")
     end
